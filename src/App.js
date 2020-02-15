1e69d0368c78405bf8ff71171f6354a04e1bda63
haibang_truong <haibang97@yahoo.com>
2020-02-16 00:28:29 +0800

    Create necessary components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import Header from './components/Header';
+import Person from './components/Person';
+import Lonely from './components/Lonely';
+import data from './data.json';
+
 function App() {
+  const [people, setPeople] = useState(data);
+  const [likedUsers, setLikedUsers] = useState([]);
+  const [superLikedUsers, setSuperLikedUsers] = useState([]);
+  const [dislikedUsers, setDislikedUsers] = useState([]);
+
+  switch (action) {
+    case 'ADD_TO_LIKED_USER':
+      break;
+    case 'ADD_TO_DISLIKED_USER':
+      break;
+    case 'ADD_TO_SUPERLIKED_USER':
+      break;
+    default:
+      return people;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      {people[1] ? (
+        <Person 
+          key = {person[1].id}
+          person = {people[1]}
+          modifySuperficialChoices = {modifySuperficialChoices}
+          likedUsers = {likedUsers}
+        />
+      ) : (
+        <Lonely
+          activeUserImage = {people[activeUser].image}
+          likedUsers={likedUsers}
+          superLikedUsers={superLikedUsers}
+        />
+      )}
     </div>
   );
 }
